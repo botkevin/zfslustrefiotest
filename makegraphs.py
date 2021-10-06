8907c7cef8f1f3e65d29988d4bd4b70de1da11ae
Shi <Kevin.Shi@kla-tencor.com>
2021-10-06 12:57:37 -0700

    make some updates to makegraphs

--- makegraphs.py
+++ makegraphs.py
@@ -2,24 +2,26 @@ import os
 import csv
 
 csvfilename = "config.csv"
-skip_num = 6
 
-def make_plot_cmd(rw, fs, bs, iodepths, numjobs, testname):
+def make_plot_cmd(rw, fs, bs, iodepths, numjobs, testname, recordsize):
     if fs == "zfs":
         fsdir = '_lustre/lustre/'
     else:
         fsdir = '_zfs/tank/'
     title = 'bandwidth of '+ rw +' on ' + fs + ' ' + testname + ' '+bs
+    if recordsize != '128k':
+        title += recordsize
     cmd = './fio-plot/fio_plot/fio_plot -T "' + title + '" -i '+ testname + fsdir +bs+' -g -r '+rw+' -t bw -d '+ iodepths +' -n ' + numjobs + ' --disable-fio-version'
     return cmd, title
 
-def make_commands():
+def make_commands(skip_num,stop=0):
     cmds = []
     with open(csvfilename) as csvfile:
         reader = csv.reader(csvfile, delimiter=",")
         next(reader)
         for _ in range(skip_num):
             next(reader)
+        stop_counter = 0
         for row in reader:
             testname, numberdisks, raidmode, raid0, ashift, compression, recordsize, atime, filesize, benchmark, runtime, blocksizes, iodepths, numjobs = row
             rws = ["read", "write"]
@@ -33,14 +35,21 @@ def make_commands():
                 cmds.append ("mkdir "+ blockdir)
                 for fs in fss:
                     for rw in rws:
-                        plot_cmd, title = make_plot_cmd (rw, fs, bs, iodepths, numjobs, testname)
+                        plot_cmd, title = make_plot_cmd (rw, fs, bs, iodepths, numjobs, testname, recordsize)
                         title = title.replace(" ", "-")
                         cmds.append (plot_cmd)
                         cmds.append ("mv " + title + '* ' + blockdir)
                         # TODO: save plot to specific dir
+            stop_counter += 1
+            if stop_counter == stop:
+                return cmds
     return cmds
 
 if __name__ == "__main__":
-    cmds = make_commands()
+    # TODO: skip_num to parameter
+    skip_num = 11
+    # TODO: stop to param
+    stop = 9
+    cmds = make_commands(skip_num, stop)
     for cmd in cmds:
         os.system (cmd)
